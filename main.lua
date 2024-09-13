76d74b0c9f8e2ca68d95a20f4278455d4372d769
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-13 20:43:39 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,34 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "script by nexer, (sus)" ,Duration = 1000, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "script by nexer" ,Duration = 1000, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
 
-wait(3)
+wait(1)
 
+local teleportFunc = queueonteleport or queue_on_teleport
+if teleportFunc then
+    teleportFunc([[
+        if not game:IsLoaded() then
+            game.Loaded:Wait()
+        end
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "script by nexer" ,Duration = 1000, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
+
+wait(1)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(247.56419372558594, -265.0000305175781, -370.77752685546875)
+wait(0.5)
 game:GetService("ReplicatedStorage").RemoteEvents.SuitUpClown:FireServer()
-wait(0.1)
-game:GetService("ReplicatedStorage").BindableEvents.KeyQuestStarted:FireServer()
 wait(0.1)
 game:GetService("ReplicatedStorage").RemoteEvents.KeyQuest:FireServer()
 wait(0.1)
-fireclickdetector(game:GetService("Workspace").QuestStuff.Key.ClickDetector)
-wait(0.1)
 game:GetService("ReplicatedStorage").RemoteEvents.KeyAcquired:FireServer()
+wait(0.1)
+game:GetService("ReplicatedStorage").RemoteEvents.GOHOME:FireServer()
 wait(0.1)
 game:GetService("ReplicatedStorage").RemoteEvents.KeyBadgeReward:FireServer()
 wait(0.1)
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "nice job",Text = "you got fan :]" ,Duration = 1000, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
+wait(1)
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "teleporting",Text = "teleporting you back to slap battles!" ,Duration = 1000, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
+wait(1)
+game:GetService("TeleportService"):Teleport(6403373529)
+	]])
+end	
+game:GetService("TeleportService"):Teleport(7234087065)
